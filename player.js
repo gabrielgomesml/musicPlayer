38b082d446437ff39dc4e1d3c24bd59299838299
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-11-29 21:04:26 -0300

    feature(js-base): add buttons functions

---
+++ player.js
@@ -0,0 +1,54 @@
+music_name = "musics/Selena Gomez - Lose You To Love Me.mp3"
+let title = document.querySelector("title1")
+let play_btn = document.querySelector("#play");
+let prev_btn = document.querySelector("#anterior");
+let next_btn = document.querySelector("#proxima");
+let range = document.querySelector("#range");
+let play_img = document.querySelector("#play_img")
+let total_time = 0;
+let currentTime = 0;
+let isPlaying = false;
+let song = new Audio();
+window.onload = playSong;
+
+function playSong(){
+    song.src = music_name;
+    console.log(song)
+    
+    
+    play_btn.addEventListener('click',function(){
+        if(!isPlaying){
+            song.play();
+            isPlaying = true;
+            total_time = song.duration;
+            range.max = total_time;
+            play_img.src = "images/pausa.svg";
+        }else{
+            song.pause();
+            isPlaying = false;
+            play_img.src = "images/play.svg";
+        }
+       song.addEventListener('ended',function(){
+            song.currentTime = 0
+            song.pause();
+            isPlaying = false;
+            range.value = 0;
+            play_img.src = "images/play.svg";
+        })
+        next_btn.addEventListener('click', function(){
+            song.currentTime = range.value;
+            song.currentTime += 10
+        })
+        prev_btn.addEventListener('click', function(){
+            song.currentTime = range.value;
+            song.currentTime -= 10
+        })
+        song.addEventListener('timeupdate',function(){
+            range.value = song.currentTime;
+        })
+        range.addEventListener('change',function(){
+            song.currentTime = range.value;
+        })
+       
+    })
+}
